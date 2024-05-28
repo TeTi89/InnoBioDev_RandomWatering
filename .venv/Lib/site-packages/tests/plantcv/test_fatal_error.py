fac35a089a73f3a99d75407d9e16b6e9ec2e0b60
TikTeK <91500957+TeTi89@users.noreply.github.com>
2024-05-28 22:01:35 +0200

    Delete .venv directory

--- .venv/Lib/site-packages/tests/plantcv/test_fatal_error.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from plantcv.plantcv import fatal_error
-
-
-def test_fatal_error():
-    """Test for PlantCV."""
-    # Verify that the fatal_error function raises a RuntimeError
-    with pytest.raises(RuntimeError):
-        fatal_error("Test error")
